18e498679bea56050596eb4d5d3bcee9301ea58d
JpabloMF <juanpablo45671@gmail.com>
2020-05-02 15:09:33 -0500

    signin, signup, dashboard initial structure

---
+++ src/screens/signin/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SignIn = (props) => {
+  return(
+  	<div>SignIn</div>
+  );
+};
+
+SignIn.propTypes = {
+  props: PropTypes.object.isRequired
+};
+
+export default SignIn;
---
+++ src/screens/signup/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SignUp = (props) => {
+  return(
+  	<div>SignUp</div>
+  );
+};
+
+SignUp.propTypes = {
+  props: PropTypes.object.isRequired
+};
+
+export default SignUp;
